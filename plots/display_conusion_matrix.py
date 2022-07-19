836e82d86156625ca19577c1c349bc40c1d4287b
Jawaher Alhosani <83540240+jawaher-is@users.noreply.github.com>
2022-07-19 16:04:49 +0400

    Update files

--- plots/display_conusion_matrix.py
+++ plots/display_conusion_matrix.py
@@ -1,4 +1,4 @@
-import argsparse
+import argparse
 import os
 import yaml
 import pandas as pd
@@ -7,7 +7,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def save_fig():
+    results_path = configuration['output_dir'] + '/results'
+    if not os.path.isdir(results_path):
+        os.makedirs(results_path)
 
+    return results_path
 
 # Get the configuration file
 parser = argparse.ArgumentParser()
@@ -15,10 +20,11 @@ parser.add_argument('--config', help='yaml configuration file path') # EXAMPLE c
 args = parser.parse_args()
 config_file = args.config
 
+# config_file = "/Users/jia2025/Desktop/XLSER-ar/content/config/wav2vec2-ksuemotions-frozen.yaml"
 with open(config_file) as f:
     configuration = yaml.load(f, Loader=yaml.FullLoader)
 print('Loaded configuration file: ', config_file)
-print(configuration)
+# print(configuration)
 
 # get file
 if ((configuration['test_corpora'] is not None) and (__name__ == '__main__')):
@@ -35,8 +41,8 @@ else:
     file_name = 'clsf_report.csv'
     cm_file_name = 'conf_matrix.csv'
 
-cm_path = configuration['output_dir'] + '/results' + cm_file_name
-cr_path = configuration['output_dir'] + '/results' + file_name
+cm_path = configuration['output_dir'] + '/results/' + cm_file_name
+cr_path = configuration['output_dir'] + '/results/' + file_name
 
 # read file as pd DataFrame
 cm_df = pd.read_csv(cm_path, sep='\t', header=0, index_col=0)
@@ -47,22 +53,29 @@ cm = ConfusionMatrixDisplay(cm_values, display_labels=labels)
 # Dispaly conf_matrix
 cm.plot()
 plt.title('Confusion Matrix')
-plt.savefig(results_path + '/' + cm_file_name.split('.')[0] + '.png')
-plt.show()
+plt.xticks(rotation=45, ha="right")
+
+results_path = save_fig()
+plt.savefig(results_path + '/' + configuration['output_dir'].split('/')[-1] + '-'+ cm_file_name.split('.')[0] + '.png', bbox_inches='tight', dpi=300)
+# plt.show()
 plt.close()
 
 
 #test
 # cr_path = "/Users/jia2025/Desktop/XLSER-ar/evaluation_results/wav2vec2-xlsr-53-arabic-ksuemotions-finetuned-3ep_clsf_report.csv"
+
 # Classification Report
 cr_df = pd.read_csv(cr_path, sep='\t', header=0, index_col=0)
 metrics = list(cr_df.columns.values)
 metrics = metrics[:-1]
 classes = list(cr_df.index.values)
+classes = classes[:-3]
 cr_values = cr_df.to_numpy()
 # remove the support column
 cr_values = np.delete(cr_values, -1, axis=1)
-#
+# remove averages
+cr_values = np.delete(cr_values, [-3,-2,-1], axis=0)
+
 cr_values = cr_values * 100
 cr_values = cr_values.round(decimals=1)
 
@@ -82,10 +95,13 @@ for i in range(len(classes)):
 
 
 plt.title('Classification Report')
-plt.rcParams['figure.figsize'] = [6, 4]
+plt.xticks(rotation=45, ha="right")
+plt.rcParams['figure.figsize'] = [6, 6]
 # plt.figure(figsize=(3, 4))
 # plt.xlabel('Metrics')
 # plt.ylabel('Classes')
-# plt.savefig(results_path + '/' + cr_file_name.split('.')[0] + '.png')
-plt.show()
+
+results_path = save_fig()
+plt.savefig(results_path + '/' + configuration['output_dir'].split('/')[-1] + '-' + file_name.split('.')[0] + '.png', bbox_inches='tight', dpi=300)
+# plt.show()
 plt.close()
